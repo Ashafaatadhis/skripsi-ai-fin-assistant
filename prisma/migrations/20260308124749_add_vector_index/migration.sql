b78324d675b11dfee44a809fa295867155be9981
Ashafaatadhis <skullreaper387@gmail.com>
2026-03-29 15:44:51 +0700

    init

--- prisma/migrations/20260308124749_add_vector_index/migration.sql
+++
@@ -1,3 +0,0 @@
--- DropIndex
-DROP INDEX "embedding_idx";
-CREATE INDEX IF NOT EXISTS "embedding_idx" ON "LongTermMemory" USING hnsw (embedding vector_cosine_ops);
--- prisma/migrations/20260308213140_add_column_type/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN     "description" TEXT,
-ADD COLUMN     "type" TEXT NOT NULL DEFAULT 'EXPENSE',
-ALTER COLUMN "merchant" DROP NOT NULL;
